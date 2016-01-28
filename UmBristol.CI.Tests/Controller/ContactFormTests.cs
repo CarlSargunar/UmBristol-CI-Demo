53bf1a12d14c7a973f91f773d9957dbc95de9a52
Carl Sargunar <carl@sargunar.com>
2016-01-28 10:03:06 +0000

    wip

---
+++ UmBristol.CI.Tests/Controller/ContactFormTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace UmBristol.CI.Tests
+{
+    [TestFixture]
+    public class ContactFormTests
+    {
+        [Test]
+        public void SomeContactFormTest()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
